ea3c92ff173ab53d27bcb34100722794e25bab53
Arjun Ambasana <arjun.gajjar23@gmail.com>
2022-04-18 21:01:42 -0400

    2nd

--- race_day.js
+++ race_day.js
@@ -1,10 +1,12 @@
-let raceNumber = Math.floor(Math.random() * 1000);
-const isEarly = true;
-const age = 18;
-if (age > 18 && isEarly){
+let raceNumber = Math.floor(Math.random() * 1000); // Decalring random number generator variable!
+const isEarly = true; // declare a variable to determine whether a participant has early registred for the race.
+
+const age = 19; //declare the age of the racer.
+
+if (age > 18 && isEarly){ // we will provide racenumbers starting from 1000 to earlybirds. 
   raceNumber+= 1000;
 }
-if (age > 18 && isEarly){
+if (age > 18 && isEarly){ // Conditional loop for every type of registration that we are accepting and deviding the participants for their respective time. 
   console.log(`Dear early racer #${raceNumber}, Your race will start at 9:30 AM. Good Luck!`);
 } else if (age > 18 && !(isEarly)) {
   console.log(`Dear racer ${raceNumber}, Your race will start at 11:00 AM.`); 
@@ -16,3 +18,5 @@ if (age > 18 && isEarly){
 else {
   console.log("Dear racer, thanks you for coming!");
 }
+
+//end of code //
